f0ad1d6e70226dc7fede14a3244d584025635400
ihleonel <ibarra.h.leonel@gmail.com>
2019-07-26 14:00:10 -0300

    Agregando logica de calculo

--- src/Calculadora.vue
+++ src/Calculadora.vue
@@ -1,32 +1,64 @@
 <template>
   <div class="calculadora">
-    <input type="text" class="input"/>
+    <div class="logs">
+      <span v-for="log in logs">{{log}}</span>
+    </div>
+    <input type="string" class="input" v-model="value" @keyup.enter="getResult()"/>
     <div class="buttons">
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>+</button>
+      <button @click="clear()">C</button>
+      <button @click="del()">del</button>
+      <button @click="addExpresion('%')">%</button>
+      <button @click="addExpresion('/')">/</button>
 
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
+      <button @click="addExpresion('7')">7</button>
+      <button @click="addExpresion('8')">8</button>
+      <button @click="addExpresion('9')">9</button>
+      <button @click="addExpresion('*')">*</button>
 
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
+      <button @click="addExpresion('4')">4</button>
+      <button @click="addExpresion('5')">5</button>
+      <button @click="addExpresion('6')">6</button>
+      <button @click="addExpresion('-')">-</button>
 
-      <button>C</button>
-      <button>0</button>
-      <button>=</button>
-      <button>/</button>
+      <button @click="addExpresion('1')">1</button>
+      <button @click="addExpresion('2')">2</button>
+      <button @click="addExpresion('3')">3</button>
+      <button @click="addExpresion('+')">+</button>
+
+      <button @click="addExpresion('0')" class="wide">0</button>
+      <button @click="addExpresion('.')">.</button>
+      <button @click="getResult()">=</button>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "calculadora",
+  data() {
+    return {
+      value: 0,
+      logs: []
+    }
+  },
+  methods: {
+    addExpresion(e) {
+      if (Number.isInteger(this.value)) {
+        this.value = '';
+      }
+      this.value += e;
+    },
+    getResult() {
+      let log = this.value;
+      this.value = eval(this.value);
+      this.logs.push(log + ' = ' + this.value);
+    },
+    clear() {
+      this.value = 0;
+    },
+    del() {
+      this.value = this.value.slice(0, -1);
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -34,6 +66,15 @@ export default {
 $orange: orange;
 .calculadora {
   width: 30%;
+  .logs {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-end;
+    background-color: #222;
+    color: white;
+  }
   .input {
     outline: 0;
     font-family: 'Dosis';
@@ -71,6 +112,10 @@ $orange: orange;
       }
       &:focus {
         outline: 0;
+      }
+
+      &.wide {
+        width: 50%;
       }
     }
   }
